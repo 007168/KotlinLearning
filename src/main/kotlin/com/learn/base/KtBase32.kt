701919ba0cd0f9995d273b9955fc441d0feb5b5b
caiqi25 <caiqi25@crv.com.cn>
2022-12-20 17:27:31 +0800

    base 32

---
+++ src/main/kotlin/com/learn/base/KtBase32.kt
@@ -0,0 +1,28 @@
+package com.learn.base
+
+// TODO 07.Kotlin语言声明变量与内置数据类型
+
+fun main() {
+
+    println("Hello World!")
+    var var1 : String = "123"
+    println(var1)
+    var1 += "456"
+    println(var1)
+    var char1 : Char = 'a'
+    println(char1)
+    /**
+     * String
+     * Char
+     * Boolean
+     * Int
+     * Double
+     * List
+     * Map
+     * Set
+     * Float
+     */
+    val val1 : String = "val12341"
+//    val1 = "222"
+    println(val1)
+}
